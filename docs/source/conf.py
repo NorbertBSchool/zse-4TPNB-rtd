8fd44bcba8b706e540f3bb7577260e1745be97d8
NorbertBSchool <nnbaczun@gmail.com>
2025-11-27 14:08:41 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,11 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'navigation_depth': 1,
+    'collapse_navigation': True,
+    'titles_only': True,
+    'includehidden': False
+}
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
